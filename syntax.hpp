71cf45f1f9e92ffdb54669aa7356868d97004438
Shubin31415926 <91184137+Shubin31415926@users.noreply.github.com>
2022-12-08 08:57:57 +0300

    Delete syntax.hpp

--- syntax.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "library.hpp"
-
-
-namespace stx 
-{
-    lib::lines_type split (const lib::line_type &line,  const char separator);
-    bool IsTrue (const lib::line_type line); //проверка корректности написания строки
-}
