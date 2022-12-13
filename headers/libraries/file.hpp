7daeddfb09f399d0e258100fae6a9774038d067c
Shubin31415926 <91184137+Shubin31415926@users.noreply.github.com>
2022-12-13 15:13:05 +0300

    Update file.hpp

--- headers/libraries/file.hpp
+++ headers/libraries/file.hpp
@@ -7,19 +7,19 @@
 
 namespace file
 {
-    void read(std::ifstream &input, std::string &line, std::string_view path);
-    void write(std::ofstream &output, const std::string &line, std::string_view path);
+    void read(std::fstream &input, std::string &line);
+    void write(std::fstream &output, const std::string &line);
 
     // template <typename Op> // Op -- operation (predicate)
     //  void stream(std::filesystem::path path, std::ios_base::openmode om, Op op);
 }
 
-void file::read(std::ifstream &input, std::string &line) //, std::string_view path)
+void file::read(std::fstream &input, std::string &line) //, std::string_view path)
 {
     input >> line;
 }
 
-void file::write(std::ofstream &output, const std::string &line) //, std::string_view path)
+void file::write(std::fstream &output, const std::string &line) //, std::string_view path)
 {
     output << line;
 }
