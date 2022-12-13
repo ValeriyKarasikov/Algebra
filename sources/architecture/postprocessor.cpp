b2b8a99489af6979b85c6687c8cd5f8b8320e70a
Shubin31415926 <91184137+Shubin31415926@users.noreply.github.com>
2022-12-13 15:02:04 +0300

    Update postprocessor.cpp

--- sources/architecture/postprocessor.cpp
+++ sources/architecture/postprocessor.cpp
@@ -1,1 +1,11 @@
-#include "postprocessor.hpp"
+#include "library.hpp"
+#include <map>
+#include "Postprocessor.hpp"
+
+//template <typename T>
+Postprocessor::Postprocessor (const lib::library_type &library) : library_{library}, commands_
+    { {"SIZE", &Postprocessor::size},
+    {"ALLNAMECONSOLE",&Postprocessor::allNameConsol},
+    {"ALLNAMEFILE",&Postprocessor::allNameFile},    
+    {"CLEARPOST",&Postprocessor::clearPost}}
+{}
