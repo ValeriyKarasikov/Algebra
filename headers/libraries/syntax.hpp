c3660bdd92c6e158a603b5614da2273e558e3bfd
Shubin31415926 <91184137+Shubin31415926@users.noreply.github.com>
2022-12-13 15:13:40 +0300

    Update syntax.hpp

--- headers/libraries/syntax.hpp
+++ headers/libraries/syntax.hpp
@@ -8,30 +8,44 @@
 #include "library.hpp"
 namespace stx
 {
-    std::list<std::string> split(std::string line, char separator); //разделяет
-    bool isBounded(std::string line, char bound);                   //с кавычками?
+    std::list<std::string> split(std::string line, char separator); // разделяет
+    bool isBounded(std::string line, char bound);                   // с кавычками?
     std::string toUppercase(std::string line);
 
     template <typename T>
     T STOI(const lib::line_type &line); // string to int
 }
 
-std::list<std::string> stx::split(std::string line, char separator)
+std::list<std::string> split(std::string line, char sep)
 {
     std::list<std::string> list;
     lib::integer_number_type i = 0;
     lib::line_type stroka;
+    char sep2 = 34;
     while (i < line.size())
     {
-        if (line[i] != separator)
+        if (line[i] == sep2)
         {
-            stroka.push_back(line[i]);
-        }
-        else
-        {
+            while (line[i] != sep2)
+            {
+                i++;
+                stroka.push_back(line[i]);
+            }
+            stroka.push_back(line[i - 1]);
             list.push_back(stroka);
             stroka.clear();
         }
+        else if (line[i] != sep)
+        {
+            stroka.push_back(line[i]);
+        }
+        else if (line[i] == sep)
+        {
+            stroka.push_back(line[i]);
+            list.push_back(stroka);
+            stroka.clear();
+        }
+        i++;
     }
     return list;
 }
@@ -52,7 +66,7 @@ bool stx::isBounded(std::string line, char bound)
 }
 
 template <typename T>
-T stx::STOI(const lib::line_type &line)// string to int
+T stx::STOI(const lib::line_type &line) // string to int
 {
     return lib::line_to_int(line);
 }
