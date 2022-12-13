65cc7505ec04fa137e14b0c6a2340ff12cb6fac9
Shubin31415926 <91184137+Shubin31415926@users.noreply.github.com>
2022-12-13 15:15:48 +0300

    Update algebra.cpp

--- sources/libraries/algebra.cpp
+++ sources/libraries/algebra.cpp
@@ -1,1 +1,45 @@
-#include "algebra.hpp"
+#include "library.hpp"
+
+lib::floating_point_type lib::line_to_float(line_type &line)
+{
+  line.pop_back();
+  return std::stof(line);
+}
+
+lib::integer_number_type lib::line_to_int(line_type &line)
+{
+  line.pop_back();
+  return std::stoi(line);
+}
+
+void lib::interpret(floating_point_type &value, line_type &string, library_type &library)
+{
+  try {
+    value = std::stof(string);
+  }
+  catch(...){
+    value = library.get<floating_point_type>(string);
+  }
+}
+
+void lib::interpret(integer_number_type &value, line_type &string, library_type &library)
+{
+  try {
+    value = std::stoi(string);
+  }
+  catch(...){
+    value = library.get<integer_number_type>(string);
+  }
+}
+
+void lib::interpret(line_type &value, line_type &string, library_type &library)
+{
+  if ((string.back()) == '"')
+  {
+    value = string;
+  }
+  else
+  {
+    value = library.get<line_type>(string);
+  }
+}
