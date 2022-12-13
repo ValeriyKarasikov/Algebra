eceae1e559a41851e00f22b9f2bf8317a498eeca
Shubin31415926 <91184137+Shubin31415926@users.noreply.github.com>
2022-12-13 15:04:08 +0300

    Update preprocessor.cpp

--- sources/architecture/preprocessor.cpp
+++ sources/architecture/preprocessor.cpp
@@ -1,1 +1,27 @@
-#include "preprocessor.hpp"
+#include "library.hpp"
+#include <map>
+#include "Preprocessor.hpp"
+#include <fstream>
+
+Preprocessor::Preprocessor(const lib::library_type &library) : library_{library}, commands_ 
+                { {"SET",&Preprocessor::set},
+                {"DEL",&Preprocessor::del},
+                {"CLEARPRE",&Preprocessor::clearPre}}
+{}
+
+
+/*
+void Preprocessor::invoke(const lib::lines_type::value_type &head, 
+                                    const lib::lines_type &others)
+{
+    if (this->commands_.find(head) !=this->commands_.end())
+    {
+        (this->*this->commands_[head])(others);
+    }
+    else
+    {
+        //throw something
+    }
+}
+ 
+*/
