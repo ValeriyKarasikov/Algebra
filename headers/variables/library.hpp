8ee08d7e0d977c8f0c3a5690f050fd25bb5b0044
Shubin31415926 <91184137+Shubin31415926@users.noreply.github.com>
2022-12-13 15:15:08 +0300

    Update library.hpp

--- headers/variables/library.hpp
+++ headers/variables/library.hpp
@@ -1,3 +1,26 @@
 #pragma once
 
-namespace lib {}
+#include <string>
+#include <list>
+
+#include "database.hpp"
+
+namespace lib
+{
+    using floating_point_type = double;
+    using integer_number_type = std::intmax_t;
+    using line_type = std::string;
+    using lines_type = std::list<line_type>;
+    using library_type = Database<floating_point_type, integer_number_type,
+                                                                line_type>;
+    using size_type = std::size_t;
+
+    floating_point_type line_to_float(line_type &line);
+    integer_number_type line_to_int(line_type &line);
+
+    void interpret(floating_point_type &value, line_type &string,
+                                               library_type &library);
+    void interpret(integer_number_type &value, line_type &string,
+                                               library_type &library);
+    void interpret(line_type &value, line_type &string, library_type &library);
+}
