abcf03e0eb2066d4d212cc713801c4abf53d3c77
Shubin31415926 <91184137+Shubin31415926@users.noreply.github.com>
2022-12-13 15:01:30 +0300

    Update interpreter.cpp

--- sources/architecture/interpreter.cpp
+++ sources/architecture/interpreter.cpp
@@ -1,1 +1,46 @@
 #include "interpreter.hpp"
+
+//Interpreter::Interpreter () : library_{}, preprocessor_{library_}
+
+
+
+
+
+
+/*
+Interpreter::Interpreter (int argc, char**argv) : library_ {
+    // {'f', 'i', '\"'}, {&stx::stof, &stx::stoi, &stx::stos}
+    },   preprocessor_{library_},   processor_{library_},   postprocessor_{library_}
+{   
+    
+    if (argc==0)
+    {
+        this->term_();
+    }
+    else
+    {
+        this->file_(argv[1]);
+    
+}
+}*/
+
+Interpreter::~Interpreter(){}
+
+
+/*
+void Interpreter::term_()
+{
+    std::string line;
+    while(line != "EXIT")
+    {
+        //term::read(line);
+    }
+}
+void Interpreter::file_(const std::string &path_to_log)
+{
+    std::string line;
+    while(line != "EXIT")
+    {
+        //file::read(line, path_to_log);
+    }
+}*/
