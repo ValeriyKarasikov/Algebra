15888b7a3cb39e512b3855367ff1fb1ffc5f65dd
Shi3oiD <117338538+Shi3oiD@users.noreply.github.com>
2022-12-21 16:52:18 +0300

    Update file.hpp

--- headers/libraries/file.hpp
+++ headers/libraries/file.hpp
@@ -1,24 +1,20 @@
 #pragma once
 
-// #include "library.hpp"
-// #include <filesystem> //прикол
 #include <fstream>
 
 namespace file
 {
-    void read(std::fstream &input, std::string &line);//, std::string_view path);
-    void write(std::fstream &output, const std::string &line);//, std::string_view path);
-
-    // template <typename Op> // Op -- operation (predicate)
-    //  void stream(std::filesystem::path path, std::ios_base::openmode om, Op op);
+    void read(std::fstream &input, std::string &line);
+    void write(std::fstream &output, const std::string &line);
+    
 }
 
-void file::read(std::fstream &input, std::string &line) //, std::string_view path)
+void file::read(std::fstream &input, std::string &line)
 {
     input >> line;
 }
 
-void file::write(std::fstream &output, const std::string &line) //, std::string_view path)
+void file::write(std::fstream &output, const std::string &line)
 {
     output << line;
 }
